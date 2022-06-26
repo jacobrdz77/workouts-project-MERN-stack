9077c32d71535b1b0fbf273357d9e721a8ed7853
jacobrdz77 <jacobrdz47@gmail.com>
2022-06-25 23:02:44 -0500

    added the base react app with a blank home page

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <header>
+      <div className="container">
+        <h1>
+          <Link to="/">Workout Buddy</Link>
+        </h1>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
