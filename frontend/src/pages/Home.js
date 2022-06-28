6180cdbe84632bbe42ec277ab8e14a18e3d1990b
jacobrdz77 <jacobrdz47@gmail.com>
2022-06-28 10:35:58 -0500

    Created workoutDetail component and added some styles for homepage

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,33 @@
-import React from "react";
+import { useEffect, useState } from "react";
+
+// components
+import WorkoutDetails from "../components/WorkoutDetails";
 
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      //The fetch to the endpoint only works for development.
+      //! Make sure that every requests points to the correct endpoints in development. */
+      const response = await fetch("/api/workouts");
+      const arrayOfWorkouts = await response.json();
+
+      if (response.ok) {
+        setWorkouts(arrayOfWorkouts);
+      }
+    };
+    fetchWorkouts();
+  }, []);
+
   return (
-    <div className="container">
-      <h2>Home</h2>
+    <div className="home">
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
+      </div>
     </div>
   );
 };
