b18acd697301d6e593a592b1435da182390c32a4
Jacob Rodriguez <jacobrdz47@gmail.com>
2022-08-11 20:17:30 -0600

    Added logging in

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,22 @@
 //import model to manipulate data in database
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.JWT_SECRET, {expiresIn: '1h'})
+}
 
 // Login User
 const loginUser = async (req, res) => {
-    res.status(200).json({msg: "log in user"})
+    const {email, password} = req.body
+    try {
+        const user = await User.login(email, password)
+        const token = createToken(user._id)
+        //Add the token to the response
+        res.status(200).json({email, token})
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
 }
 
 // Signup User
@@ -11,7 +24,11 @@ const signupUser = async (req, res) => {
     const {email, password} = req.body
     try {
         const user = await User.signup(email, password)
-        res.status(200).json(user)
+        // Create a token
+            // The token has a header, payload, and secret.
+        const token = createToken(user._id)
+        //Add the token to the response
+        res.status(200).json({email, token})
     } catch (error) {
         res.status(400).json({error: error.message})
     }
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 
 const userSchema = new mongoose.Schema({
@@ -18,6 +19,17 @@ const userSchema = new mongoose.Schema({
 // We can also make our own static methods for each model.
 // Static Signup Method
 userSchema.statics.signup = async function(email, password) {
+    //validation
+    if(!email || !password){
+        throw Error("All fields must be filled")
+    }
+    if(!validator.isEmail(email)){
+        throw Error('Email is not valid')
+    }
+    if(!validator.isStrongPassword(password)){
+        throw Error('Password must have at least one digit, one uppercase letter, and one special character. ')
+    }
+
     //Check if email is already in database
     const exists = await this.findOne({email})
     if(exists) {
@@ -33,7 +45,27 @@ userSchema.statics.signup = async function(email, password) {
     })
 
     return user;
-
 } 
+
+userSchema.statics.login = async function(email, password) {
+    // Either one are empty
+    if(!email || !password){
+        throw Error("All fields must be filled")
+    }
+
+    //Check if email is already in database
+    const user = await this.findOne({email})
+    if(!user) {
+        throw Error('Incorrect email ')
+    }
+
+    // Compare both passwords
+    const match = await bcrypt.compare(password, user.password)
+    if (!match){
+        throw Error('Incorrect password')
+    }
+
+    return user
+}
 
 module.exports = mongoose.model('User', userSchema)
